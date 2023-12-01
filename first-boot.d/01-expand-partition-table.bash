27fd2d020eb9bcaabe35ff3d69dd3c75338d9b34
Cyan <cyanoxygen@aosc.io>
2023-12-01 11:40:08 +0800

    first-boot.d: Finish partition table expand step

--- first-boot.d/01-expand-partition-table.bash
+++ first-boot.d/01-expand-partition-table.bash
@@ -1,45 +1,84 @@
 #!/bin/bash
-# Devona - AOSC OS Device support task force
+# Devena - AOSC OS Device support task force
 # first-boot.d - Device specific first boot configuration
 # expand-partition-table - automatically expand the partition table.
 
-ROOTPART=$(findmnt -lno SOURCE /)
-ROOTDEV="$(lsblk -lno PKNAME $ROOTPART)"
-eval $(blkid -o export $ROOTDEV)
-
-# TODO implement basic sanity checks, e.g. device-mapper.
-
-# Resize the partition table
-# Only affects GPT. Just to make sure.
-echo '' | sudo sfdisk -f $ROOTDEV
-
-# Reload the partition table.
-partprobe $ROOTDEV
-
-# Check if the root partition is the last partition on the disk.
-PARTS=$(lsblk -lno NAME $ROOTDEV)
-if [ "${ROOTPART/\/dev\//}" != "${PARTS[-1]}" ] ; then
-	echo "[!] Root partition is not the last partition on the disk."
-	echo "    Can't perform the resize. Skipping."
+# SOURCE = /dev/some_blkdevpN
+eval $(findmnt -Pyo SOURCE /)
+# NAME = some_blkdevpN, PKNAME = some_blkdev
+eval $(lsblk -Pydno NAME,PKNAME $SOURCE)
+if [ ! -e /dev/$ROOTPART ] || [ ! -e "$ROOTPART_PATH" ] ; then
+	echo "[!] Root partition is not a physical partition. Skipping."
 	exit 0
 fi
+# DEVNAME = /dev/some_blkdev, PTUUID = partition table UUID, PTTYPE = gpt|dos
+eval $(blkid -o export /dev/$PKNAME || echo "")
+ROOTPART=$NAME
+ROOTPART_PATH=$(realpath -q $SOURCE)
+ROOTDEV=$PKNAME
+ROOTDEV_PATH=$DEVNAME
 
-# Resize the root partition
-echo ",+" | sfdisk -f -N $PARTNUM $ROOTDEV
-partprobe $ROOTDEV
+[ -e /etc/default/devena ] && source /etc/default/devena
 
-case "$TYPE" in
-	ext4)
-		resize2fs $ROOTPART
-		;;
-	xfs)
-		xfs_growfs $ROOTPART
-		;;
-	btrfs)
-		btrfs filesystem resize $ROOTPART
-		;;
-	*)
-		echo "[!] Unsupported filesystem: $TYPE. Skipping."
-		exit 0;
-esac
+resize_partition_table() {
+	echo "[+] Expanding the partition table..."
+	# Resize the partition table
+	# This does noting to the partitoin table, except expanding it to the whole
+	# disk. It does this automatically on write. Only affects GPT.
+	echo '' | sudo sfdisk -f $ROOTDEV
+	
+	# Reload the partition table.
+	partprobe $ROOTDEV
+	
+	# Check if the root partition is the last partition on the disk.
+	PARTS=($(lsblk -lno NAME $ROOTDEV))
+	if [ "$ROOTPART" != "${PARTS[-1]}" ] ; then
+		echo "[!] Root partition is not the last partition on the disk."
+		echo "    Can't perform the resize. Skipping."
+		exit 0
+	fi
+}
+
+resize_root_partition() {
+	echo "[+] Expanding the root filesystem..."
+	# If the root filesystem is a physical partition
+	if [ -e /sys/class/block/$ROOTPART/partition ] ; then
+		PARTNUM=$(cat /sys/class/block/$ROOTPART/partition)
+		# Resize the root partition
+		echo ",+" | sfdisk -f -N $PARTNUM $ROOTDEV
+		partprobe $ROOTDEV
+	fi
+	
+	case "$TYPE" in
+		ext4)
+			resize2fs $ROOTPART_PATH
+			;;
+		xfs)
+			xfs_growfs $ROOTPART_PATH
+			;;
+		btrfs)
+			btrfs filesystem resize $ROOTPART_PATH
+			;;
+		*)
+			echo "[!] Unsupported filesystem: $TYPE. Skipping."
+			exit 0
+			;;
+	esac
+}
+
+# Basic sanity checks
+# Check if the root partition is a Device Mapper node.
+# We can expand the root filesystem, but mostly it is already the size of
+# the logical volume.
+if [[ "$ROOTPART_PATH" = \/dev\/dm* ]] ; then
+	echo "[!] Root partition is possibly a logical volume. Skipping expanding"
+	echo "    the partition table."
+# If the following is true, then the root partition is probably a NFS mount
+# or iSCSI target.
+elif [ "$RESIZE_PARTITION_TABLE" ] ; then
+	resize_partition_table
+fi
+
+resize_root_partition
+
 echo "[+] Finished."
