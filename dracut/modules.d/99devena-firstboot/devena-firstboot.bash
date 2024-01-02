8110490d0129b5a3e4b20a8923ba2d3064e917b6
Cyan <cyan@cyano.uk>
2024-01-02 03:04:51 +0000

    dracut: source devena-utils.bash; more output

--- dracut/modules.d/99devena-firstboot/devena-firstboot.bash
+++ dracut/modules.d/99devena-firstboot/devena-firstboot.bash
@@ -4,12 +4,22 @@
 # Terminate plymouth first
 type plymouth > /dev/null 2>&1 && plymouth quit
 
-source /usr/lib/devena-lib/initrd-lib.bash
+source /usr/lib/devena-lib/devena-utils.bash
+
+echo -e "\033[1m========================================"
+echo           "        AOSC OS First Boot Setup"
+echo           "========================================"
+
+info "Welcome to AOSC OS!"
+info "The first boot setup is taking place, please wait."
+sleep 5
 
 for f in /usr/lib/devena-lib/first-boot.d/*.bash ; do
-	source $i
+	source $f
+	sleep 1
 done
 
-info "Done! Rebooting in 10 seconds."
+info "The first boot setup finished successfully!"
+info "Thank you for choosing AOSC OS. Rebooting in 10 seconds."
 sleep 10
 reboot
