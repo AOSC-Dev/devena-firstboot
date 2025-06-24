2bd24184f315cd4218594548474884e285b67812
Xinhui Yang <cyan@cyano.uk>
2025-06-24 11:26:45 +0800

    chore: renumber scripts to allow more flexibility
    With only two digits it is hard to insert custom procedures betweem the
    predefined workflow.

--- rpi/first-boot.d/files.mk
+++ rpi/first-boot.d/files.mk
@@ -2,16 +2,16 @@
 #
 # firstboot-general: First boot scripts in global space
 files-firstboot-general = \
-	00-find-rootdev.bash \
-	11-expand-partition-table.bash \
-	12-randomize-partition-table-uuid.bash \
-	13-expand-rootfs.bash \
-	14-randomize-fsuuid.bash \
-	20-mount-rootdev.bash \
-	21-allocate-swapfile.bash \
-	90-generate-fstab.bash \
-	99-umount-sysroot.bash
+	0100-find-rootdev.bash \
+	1010-expand-partition-table.bash \
+	1020-randomize-partition-table-uuid.bash \
+	1030-expand-rootfs.bash \
+	1040-randomize-fsuuid.bash \
+	2010-mount-rootdev.bash \
+	2020-allocate-swapfile.bash \
+	9900-generate-fstab.bash \
+	9990-umount-sysroot.bash
 
 # firstboot-rpi: First boot scripts specific to Raspberry Pi
 files-firstboot-rpi = \
-	22-mount-boot-partition.bash
+	2011-mount-boot-partition.bash
