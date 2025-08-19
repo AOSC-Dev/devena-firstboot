b370ae33c66dfedfcd9ae96bda4141e1a346070c
Xinhui Yang <cyan@cyano.uk>
2025-08-19 13:48:22 +0800

    update-grub: reinstall grub first

--- first-boot.d/9910-update-grub.bash
+++ first-boot.d/9910-update-grub.bash
@@ -1,6 +1,8 @@
 update_grub() {
 	info "Generating new initramfs and GRUB configuration ..."
 	arch-chroot $TARGET_SYSROOT/ \
+		grub-install --bootloader-id="AOSC OS"
+	arch-chroot $TARGET_SYSROOT/ \
 		update-initramfs
 	msg "Done."
 }
