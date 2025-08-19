9fd294d9290e6cf23a0db94afcf01982e57dd40d
Xinhui Yang <cyan@cyano.uk>
2025-08-19 13:48:54 +0800

    first-boot.d: add routine for mounting the ESP partition
    Device maintainers should use this routine carefully. It is better for
    them to write their own version instead of this one.

--- generic/first-boot.d/files.mk
+++ generic/first-boot.d/files.mk
@@ -8,6 +8,7 @@ files-firstboot-general = \
 	1030-expand-rootfs.bash \
 	1040-randomize-fsuuid.bash \
 	2010-mount-rootdev.bash \
+	2011-mount-esp.bash \
 	2020-allocate-swapfile.bash \
 	9900-generate-fstab.bash \
 	9910-update-grub.bash \
