8e6da8066146c2263954cfcdd51211ee4445d5a7
Xinhui Yang <cyan@cyano.uk>
2025-08-28 22:58:38 +0800

    update-grub: remove devena-firstboot initramfs image

--- first-boot.d/9910-update-grub.bash
+++ first-boot.d/9910-update-grub.bash
@@ -1,4 +1,8 @@
 update_grub() {
+	info "Removing devena-firstboot.img ..."
+	if [ -e "$TARGET_SYSROOT"/boot/devena-firstboot.img ] ; then
+		rm "$TARGET_SYSROOT"/boot/devena-firstboot.img
+	fi
 	info "Generating new initramfs and GRUB configuration ..."
 	arch-chroot $TARGET_SYSROOT/ \
 		grub-install --bootloader-id="AOSC OS"
